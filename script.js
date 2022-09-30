5b0712e2e90b2c29087bd6a059a0d478a78d1ef0
rakpcodes <rakpcodes@gmail.com>
2022-09-30 16:17:33 -0400

    added functionallity for open and close modal

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+/* 
+
+Psuedo 🧪:
+
+- Store elements within variables
+- For each modal button
+    - have overlay and modal appear
+    - add functionality to close button and overlay
+
+
+*/
+
+
+// VARIABLES
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const closeModal = document.querySelector('.close-modal')
+const openModal = document.querySelectorAll('.show-modal')
+
+
+
+for (let i = 0; i < openModal.length; i++) {
+
+    openModal[i].addEventListener('click', function () {
+
+        overlay.classList.remove('hidden')
+        modal.classList.remove('hidden')
+
+        closeModal.addEventListener('click', function () {
+
+            overlay.classList.add('hidden')
+            modal.classList.add('hidden')
+
+        })
+
+        overlay.addEventListener('click', function () {
+
+            overlay.classList.add('hidden')
+            modal.classList.add('hidden')
+
+        })
+
+    })
+
+}
